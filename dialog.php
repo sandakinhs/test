1c55a0030de27c60f49fd028eb8cfb1b20616ce7
sandakinhs <sandakinhs@gmail.com>
2015-09-22 13:33:58 +0530

    bug fix

--- dialog.php
+++ dialog.php
@@ -7,24 +7,41 @@
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  
+  #dialog .ui-dialog-titlebar
+{
+    display:none;
+}
+  </style>
   <script>
   $(function() {
     $( "#dialog" ).dialog({
       width: 400,
       height: 400,
-      position: { my: 'top', at: 'top+150' },
-      modal: true,
+      position: { my: 'right-230 center'},
+      // modal: true,
       resizable: false,
-      closeOnEscape: false
+      closeOnEscape: false,
+      
     });
-    $( "#dialog1" ).dialog();
+
+
+    $( "#dialog1" ).dialog({
+      width: 400,
+      height: 400,
+      position: { my: 'center' },
+      // modal: true,
+      resizable: false,
+      closeOnEscape: false,
+    });
   });
   </script>
 </head>
 <body>
  
 <div id="dialog" title="Basic dialog">
-  <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+ <img src="day.png" align="right">
 </div>
 
 <div id="dialog1" >
